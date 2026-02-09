381c05a489f83d26fc60dcc747ed1b84a865f3c8
ALupic <alexlu_1995@hotmail.com>
2026-02-09 21:03:03 +0100

    Room database - Destination setup

--- app/src/main/java/com/example/gobeyond/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/gobeyond/navigation/AppNavGraph.kt
@@ -1,17 +1,22 @@
 package com.example.gobeyond.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.gobeyond.ui.data.AppDatabaseProvider
 import com.example.gobeyond.ui.data.DestinationRepository
+import com.example.gobeyond.ui.destinations.DestinationScreen
 import com.example.gobeyond.ui.explore.CountryListScreen
 import com.example.gobeyond.ui.explore.CountryViewModel
 import com.example.gobeyond.ui.explore.DestinationListScreen
 import com.example.gobeyond.ui.explore.DestinationViewModel
+
 
 @Composable
 fun AppNavGraph(
@@ -32,7 +37,13 @@ fun AppNavGraph(
             )
         }
 
-        composable("destinations/{countryId}") { backStackEntry ->
+        composable(
+            route = "destinations/{countryId}",
+            arguments = listOf(
+                navArgument("countryId") { defaultValue = "" }
+            )
+        ) { backStackEntry ->
+
             val countryId = backStackEntry.arguments?.getString("countryId") ?: ""
 
             val db = AppDatabaseProvider.createDatabase(LocalContext.current)
@@ -42,7 +53,25 @@ fun AppNavGraph(
                 DestinationViewModel(repository, countryId)
             }
 
-            DestinationListScreen(viewModel)
+            val destinations by viewModel.destinations.collectAsState()
+
+            DestinationListScreen(
+                destinations = destinations,
+                onDestinationClick = { destinationName ->
+                    navController.navigate("destination/$destinationName")
+                }
+            )
         }
+
+        composable(
+            route = "destination/{name}",
+            arguments = listOf(
+                navArgument("name") { defaultValue = "" }
+            )
+        ) { backStackEntry ->
+            val name = backStackEntry.arguments?.getString("name") ?: ""
+            DestinationScreen(destinationName = name)
+        }
+
     }
 }
