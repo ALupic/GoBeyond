6da89d5e48e53bc7f8c1b8a9fd9744d5db055e26
ALupic <alexlu_1995@hotmail.com>
2026-03-22 12:42:40 +0100

    Hero screen

--- app/src/main/java/com/example/gobeyond/ui/explore/ExploreScreen.kt
+++ app/src/main/java/com/example/gobeyond/ui/explore/ExploreScreen.kt
@@ -1,50 +1,77 @@
 package com.example.gobeyond.ui.explore
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gobeyond.ui.theme.GoBeyondTheme
+import com.example.gobeyond.R
 
 @Composable
-fun ExploreScreen(
-    onByCountryClicked: () -> Unit = {}
-    //onByTypeClicked: () -> Unit = {}
-){
-    Column(
+fun ExploreScreen(){
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = "Explore Destinations")
+    ) {
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Image(
+            painter = painterResource(id = R.drawable.noto), // add your image
+            contentDescription = "Blog cover",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
 
-        Button(onClick = {onByCountryClicked()}){
-            Text(text = "Explore By Country")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.4f)
+                .align(Alignment.BottomCenter)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.8f)
+                        )
+                    )
+                )
+        )
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(24.dp)
+        ) {
+            Text(
+                text = "Discover",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White
+            )
+
+            //Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Noto",
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White
+            )
+
+            Text(
+                text = "Explore Baroque palaces, ornate cathedral and coastal charms",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White
+            )
         }
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        /*Button(onClick = {onByTypeClicked()}){
-            Text(text = "Explore By Type")
-        }*/
-
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun ExploreScreenPreview(){
-    GoBeyondTheme{
-        ExploreScreen(){
-
-        }
-    }
-}
